fb764eaf63b2edc3077e706f7b43b7aa6d121ac4
Cam Macdonell <cam@cs.ualberta.ca>
2009-09-22 15:40:23 -0600

    Updated my JNI code from the old repo

--- tests/Java/JNI/MemAccess.cpp
+++ tests/Java/JNI/MemAccess.cpp
@@ -1,15 +1,48 @@
 // MemAccess.cpp:
 #include "MemAccess.h"
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <openssl/sha.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+
+int global_fd;
 
 JNIEXPORT jobject JNICALL Java_MemAccess_getGeneratedMem(JNIEnv * env, jobject obj) {
-    int nbr = 16;
-    int * buffer = new int[nbr]; // c++ buffer
-    int i;
+    int sz = 16 * 1024 * 1024;
+    int i, nbr;
 
-    for (i = 0; i < nbr; i++)
-        buffer[i] = i * 2;
+    nbr = sz / sizeof(long);
+    long * buffer = new long[nbr]; // c++ buffer
+
+    global_fd=open("/dev/ivshmem", O_RDWR);
+    printf("fd is %d\n", global_fd);
+
+    if ((buffer = (long *)mmap(NULL, sz, PROT_READ|PROT_WRITE, MAP_SHARED, global_fd, 0)) == (long *)(caddr_t)-1){ 
+        printf("mmap failed (0x%x)\n", buffer);
+        close (global_fd);
+        exit (-1);
+    }
+
+
+    for (i = 0; i < 16; i++)
+        printf("%ld\n", buffer[i]);
+    printf("%ld\n", buffer[nbr-1]);
+    printf("**************\n");
 
     // return a reference of a (Java) object that holds a pointer to the c++ buffer
-    return env->NewDirectByteBuffer(buffer, nbr * sizeof(int));
+    return env->NewDirectByteBuffer(buffer, nbr * sizeof(long));
 }
 
+JNIEXPORT void JNICALL Java_MemAccess_closeGeneratedMem(JNIEnv * env, jobject obj) {
+
+    printf("global_fd is %d\n", global_fd);
+    close(global_fd);
+
+}
--- tests/Java/JNI/MemAccess.java
+++ tests/Java/JNI/MemAccess.java
@@ -1,27 +1,56 @@
 // MemAccess.java:
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.security.MessageDigest;
  
 public class MemAccess {
     static {
         System.loadLibrary("MemAccess");
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         new MemAccess().readGeneratedMem();
     }
     
-    void readGeneratedMem() {
+    void readGeneratedMem() throws Exception {
         // get a reference to the object that holds a pointer to the c++ buffer
         ByteBuffer buffer = getGeneratedMem();
         buffer.order(ByteOrder.nativeOrder()); 
-        int nbr = 16;
+        long nbr = 16;
         // walk through the c++ buffer to display the values
-        for (int i = 0; i < nbr; i++)
-            System.out.println(buffer.getInt());
+//        for (int i = 0; i < nbr; i++)
+//            System.out.println(buffer.getLong());
+    
+	closeGeneratedMem();
+
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+        md.update(buffer);
+        byte[] bytes = md.digest();
+        System.out.println(HexConversions.bytesToHex(bytes).toLowerCase());
+
+	buffer.position(0);
+	md.reset();
+        md.update(buffer);
+        bytes = md.digest();
+
+        System.out.println(HexConversions.bytesToHex(bytes).toLowerCase());
+
     }
     
     native ByteBuffer getGeneratedMem();
-}
+    native void closeGeneratedMem();
  
+	private static final String toHex(int s) {
+		if (s < 10) {
+		   return new StringBuffer().
+                                append((char)('0' + s)).
+                                toString();
+		} else {
+		   return new StringBuffer().
+                                append((char)('A' + (s - 10))).
+                                toString();
+		}
+	}
 
+}
