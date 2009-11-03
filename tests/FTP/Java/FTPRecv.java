44629978db2f2a9cb8a36aea2d670f4dacb05f8c
Adam Wolfe Gordon <adam.wolfegordon@gmail.com>
2009-11-03 15:57:42 -0700

    Timing.

--- tests/FTP/Java/FTPRecv.java
+++ tests/FTP/Java/FTPRecv.java
@@ -19,36 +19,38 @@ public class FTPRecv extends FTP {
         recvfile = args[1];
         sender = Integer.parseInt(args[2]);
 
-        System.out.println("[RECV] Opening device " + devname);
+        System.out.println("[RECV] Starting at " + String.valueOf(System.currentTimeMillis));
+
+        //System.out.println("[RECV] Opening device " + devname);
         mem = new MemAccess(devname);
         FileOutputStream file = new FileOutputStream((new File(recvfile)).getName());
 
         /* Indicate our interest */
         me = mem.getPosition();
-        System.out.println("[RECV] I am VM number " + String.valueOf(me) + " receiving from VM number " + String.valueOf(sender));
+        //System.out.println("[RECV] I am VM number " + String.valueOf(me) + " receiving from VM number " + String.valueOf(sender));
         while(mem.spinLock(SYNC(sender) + SLOCK) != 0);
         mem.writeInt(me, SYNC(sender) + CLIENT);
         mem.waitEventIrq(sender);
-        System.out.println("[RECV] Waiting for the server to write a block number.");
+        //System.out.println("[RECV] Waiting for the server to write a block number.");
         mem.waitEvent();
 
         /* Read the block number and write the filename */
         block = mem.readInt(SYNC(sender) + BLK);
-        System.out.println("[RECV] Will be using block " + String.valueOf(block));
+        //System.out.println("[RECV] Will be using block " + String.valueOf(block));
         fname = recvfile.getBytes();
         mem.writeBytes(fname, BASE(block) + FNAME, fname.length);
         mem.waitEventIrq(sender);
-        System.out.println("[RECV] Waiting for size from sender.");
+        //System.out.println("[RECV] Waiting for size from sender.");
         mem.waitEvent();
         
         /* Read the size */
         total = mem.readLong(BASE(block) + SIZE);
-        System.out.println("[RECV] Got size from sender: " + String.valueOf(total));
+        //System.out.println("[RECV] Got size from sender: " + String.valueOf(total));
         mem.waitEventIrq(sender);
 
         recvd = 0;
         for(idx = 0; recvd < total; idx = NEXT(idx)) {
-            System.out.println("[RECV] Waiting for full slot.");
+            //System.out.println("[RECV] Waiting for full slot.");
             do {
                 Thread.sleep(50);
                 full = mem.readInt(BASE(block) + FULL);
@@ -58,27 +60,29 @@ public class FTPRecv extends FTP {
             full = full - 1;
             mem.writeInt(full, BASE(block) + FULL);
             mem.spinUnlock(BASE(block) + FLOCK);
-            System.out.println("[RECV] Decremented full to " + String.valueOf(full));
+            //System.out.println("[RECV] Decremented full to " + String.valueOf(full));
 
             mem.readBytes(bytes, OFFSET(block, idx), CHUNK_SZ);
             file.write(bytes, 0, CHUNK_SZ);
             recvd += CHUNK_SZ;
-            System.out.println("[RECV] Read bytes in slot " + String.valueOf(idx) + " recvd =  " + String.valueOf(recvd));
+            //System.out.println("[RECV] Read bytes in slot " + String.valueOf(idx) + " recvd =  " + String.valueOf(recvd));
 
             while(mem.spinLock(BASE(block) + ELOCK) != 0);
             empty = mem.readInt(BASE(block) + EMPTY);
             empty = empty + 1;
             mem.writeInt(empty, BASE(block) + EMPTY);
             mem.spinUnlock(BASE(block) + ELOCK);
-            System.out.println("[RECV] Incremented empty to " + String.valueOf(empty));
+            //System.out.println("[RECV] Incremented empty to " + String.valueOf(empty));
         }
        
-        System.out.println("[RECV] Done, closing file and device.");
+        //System.out.println("[RECV] Done, closing file and device.");
         file.getChannel().truncate(total);
         file.close();
 
         mem.spinUnlock(SYNC(sender) + SLOCK);
 
         mem.closeDevice();
+
+        System.out.println("[RECV] Done at " + String.valueOf(System.currentTimeMillis));
     }
 }
