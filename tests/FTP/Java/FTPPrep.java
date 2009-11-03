abe679f022586c8f9e51e5f94946941b4ac2015c
Adam Wolfe Gordon <adam.wolfegordon@gmail.com>
2009-11-03 16:43:37 -0700

    OK, now.

--- tests/FTP/Java/FTPPrep.java
+++ tests/FTP/Java/FTPPrep.java
@@ -1,7 +1,7 @@
 public class FTPPrep extends FTP {
     public static void main(String args[]) throws Exception {
         MemAccess mem = new MemAccess(args[0]);
-        for(int i = 1; i < 32; i++) {
+        for(int i = 1; i < 8; i++) {
             mem.initLock(SYNC(i) + SLOCK);
             mem.initLock(BASE(i - 1) + LOCK);
         }
